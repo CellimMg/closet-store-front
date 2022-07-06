e9d332c32dd6dc1e08ea8cc6b7462b4e6da783df
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-07-06 17:56:48 -0300

    Feat: Sign screen is working

--- src/components/signin/index.jsx
+++ src/components/signin/index.jsx
@@ -1,9 +1,60 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import { ThreeDots } from 'react-loader-spinner';
 
-export default function Signin (){
+
+export default function Signin({setUserData}) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  function login(event) {
+    event.preventDefault();
+    setLoading(true);
+    const LINK_API = "http://localhost:5000/signin";
+    const request = axios.post(LINK_API, {
+      email,
+      password
+    });
+    request.then(response => {
+      const { data } = response;
+      setUserData(data);
+      console.log(data);
+    })
+    request.catch(err => {
+      console.log(err.response);
+      setLoading(false);
+      alert("E-mail ou senha incorretos. Tente novamente.");
+    });
+  }
+
   return (
+    <div>
+      < form onSubmit={login} >
+        <input
+          type="email"
+          disabled={loading ? true : false}
+          placeholder="E-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} />
 
-    <h1>teste</h1>
+        <input
+          type="password"
+          disabled={loading ? true : false}
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} />
 
+        <button type="submit">
+          {loading ? (
+            <ThreeDots color="#00ffff" height={13} align='center' />
+          ) : (
+            'Entrar'
+          )}
+        </button>
+      </form >
+    </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from 'react';
 
 import Signin from "./components/signin";
+import UserContext from "./context/UserContext";
 
 
 function App() {
+  const [userData, setUserData] = useState({});
 
   return (
+    <UserContext.Provider value={{ userData, setUserData }}>
       <BrowserRouter>
         <Routes>
-          <Route path="/signin" element={<Signin />} />
+          <Route path="/signin" element={<Signin setUserData={setUserData}/>} />
         </Routes>
       </BrowserRouter>
+    </UserContext.Provider>
   )
 }
 
